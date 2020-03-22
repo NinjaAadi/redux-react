4779adb528d28d0d1c8d73cf737d3e1a5d47b799
NinjaAadi <aaditya7739008423@gmail.com>
2020-03-22 10:31:10 +0530

    second file

---
+++ index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+ import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+
+import Reducer from './store/reducer';
+const store = createStore(Reducer);
+ReactDOM.render(<Provider store={store}><App /></Provider>,document.getElementById("root"));
